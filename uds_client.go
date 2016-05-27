34db30f6fd848f4213d5542061e7fe1af37f4f62
nareshsankapellyfkrt <naresh.sankapelly@flipkart.com>
2016-05-27 11:46:19 +0530

    Using unit64 instead of int

--- uds_client.go
+++ uds_client.go
@@ -14,6 +14,18 @@ import (
 	"time"
 )
 
+// Uint64Slice attaches the methods of sort.Interface to []uint64, sorting in increasing order.
+type Uint64Slice []uint64
+
+func (s Uint64Slice) Len() int           { return len(s) }
+func (s Uint64Slice) Less(i, j int) bool { return s[i] < s[j] }
+func (s Uint64Slice) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+
+// Sort is a convenience method.
+func (s Uint64Slice) Sort() {
+	sort.Sort(s)
+}
+
 func createByteArray(size int) []byte {
 	var buffer = make([]byte, size)
 	for i := range buffer {
@@ -22,7 +34,7 @@ func createByteArray(size int) []byte {
 	return buffer
 }
 
-func writeToUdsSocket(c net.Conn, wg *sync.WaitGroup, numberOfMessages int, dataSize int, latencies []int, index int) {
+func writeToUdsSocket(c net.Conn, wg *sync.WaitGroup, numberOfMessages int, dataSize int, latencies []uint64, index int) {
 	for i := 0; i < numberOfMessages; i++ {
 		t0 := time.Now()
 		_, err := c.Write(createByteArray(dataSize))
@@ -31,7 +43,7 @@ func writeToUdsSocket(c net.Conn, wg *sync.WaitGroup, numberOfMessages int, data
 			break
 		}
 		t1 := time.Now()
-		latencies[index*numberOfMessages+i] = int(t1.Sub(t0).Nanoseconds() / 1000)
+		latencies[index*numberOfMessages+i] = uint64(t1.Sub(t0).Nanoseconds() / 1000)
 	}
 	wg.Done()
 }
@@ -109,23 +121,23 @@ func main() {
 
 	defer writer.Flush()
 
-	numberOfMessages := 10000
-	for connectionPoolSize := 8; connectionPoolSize <= maxConnectionPoolSize; connectionPoolSize = connectionPoolSize * 2 {
+	numberOfMessages := 5000
+	for connectionPoolSize := 32; connectionPoolSize <= maxConnectionPoolSize; connectionPoolSize = connectionPoolSize * 2 {
 		connectionPool := createConnectionPool(connectionPoolSize)
-		for numOfThreads := 8; numOfThreads <= maxConcurrency; numOfThreads = numOfThreads * 2 {
-			for dataSize := 64; dataSize <= maxSize; dataSize = dataSize * 4 {
+		for numOfThreads := 16; numOfThreads <= maxConcurrency; numOfThreads = numOfThreads * 2 {
+			for dataSize := 128; dataSize <= maxSize; dataSize = dataSize * 4 {
 				var wg sync.WaitGroup
 				t0 := time.Now()
-				var latencies = make([]int, numOfThreads*numberOfMessages)
+				var latencies = make(Uint64Slice, numOfThreads*numberOfMessages)
 				for i := 0; i < numOfThreads; i++ {
 					wg.Add(1)
 					go writeToUdsSocket(connectionPool[rand.Intn(len(connectionPool))], &wg, numberOfMessages, dataSize, latencies, i)
 				}
 				wg.Wait()
 				t1 := time.Now()
-				var sumOfLatencies int = 0
-				var maxLatency int = latencies[0]
-				var minLatency int = latencies[0]
+				var sumOfLatencies uint64 = 0
+				var maxLatency uint64 = latencies[0]
+				var minLatency uint64 = latencies[0]
 				for _, value := range latencies {
 					if value > maxLatency {
 						maxLatency = value
@@ -137,31 +149,32 @@ func main() {
 
 					sumOfLatencies = sumOfLatencies + value
 				}
-				sort.Ints(latencies)
+				//sort.Ints(latencies)
+				latencies.Sort()
 				numberOfWrites := (numOfThreads * numberOfMessages)
-				averageLatency := float64(sumOfLatencies / numberOfWrites)
-				var timeTakenInMilliSecs int64 = t1.Sub(t0).Nanoseconds() / (1000 * 1000)
-				var throughput int64 = (int64(numberOfWrites*1000) / timeTakenInMilliSecs)
+				averageLatency := float64(sumOfLatencies / uint64(numberOfWrites))
+				var timeTakenInMilliSecs uint64 = uint64(t1.Sub(t0).Nanoseconds() / (1000 * 1000))
+				var throughput uint64 = (uint64(numberOfWrites*1000) / timeTakenInMilliSecs)
 				var nintyNinthPercentileIndex int = int((99 * numberOfWrites) / 100)
 				var nintyFifthPercentileIndex int = int((95 * numberOfWrites) / 100)
 
-				var nintyNinthPercentileLatency int = latencies[nintyNinthPercentileIndex]
-				var nintyFifthPercentileLatency int = latencies[nintyFifthPercentileIndex]
+				var nintyNinthPercentileLatency uint64 = uint64(latencies[nintyNinthPercentileIndex])
+				var nintyFifthPercentileLatency uint64 = uint64(latencies[nintyFifthPercentileIndex])
 				record := []string{strconv.Itoa(connectionPoolSize),
 					strconv.Itoa(numOfThreads),
 					strconv.Itoa(dataSize),
 					strconv.FormatFloat(float64(averageLatency), 'f', 2, 64),
-					strconv.FormatFloat(float64(nintyNinthPercentileLatency), 'f', 2, 64),
-					strconv.FormatFloat(float64(nintyFifthPercentileLatency), 'f', 2, 64),
-					strconv.FormatFloat(float64(throughput), 'f', 2, 64)}
+					strconv.FormatUint(nintyNinthPercentileLatency, 10),
+					strconv.FormatUint(nintyFifthPercentileLatency, 10),
+					strconv.FormatUint(throughput, 10)}
 
 				writer.Write(record)
 
 				fmt.Printf("conns:%d, Conc:%d, size:%d bytes,  throughput:%s, averageLatency:%s us, 99thPercentileLatency:%s us, 95thPercentileLatency: %s us\n",
-					connectionPoolSize, numOfThreads, dataSize, strconv.FormatFloat(float64(throughput), 'f', 2, 64),
+					connectionPoolSize, numOfThreads, dataSize, strconv.FormatUint(throughput, 10),
 					strconv.FormatFloat(float64(averageLatency), 'f', 2, 64),
-					strconv.FormatFloat(float64(nintyNinthPercentileLatency), 'f', 2, 64),
-					strconv.FormatFloat(float64(nintyFifthPercentileLatency), 'f', 2, 64))
+					strconv.FormatUint(nintyNinthPercentileLatency, 10),
+					strconv.FormatUint(nintyFifthPercentileLatency, 10))
 			}
 		}
 	}
